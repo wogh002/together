abe665ffbd39f66f7bd51befd4def7cd35f2f9b6
Song Hyeon Yang <54925535+thdgus90@users.noreply.github.com>
2021-10-28 14:08:24 +0900

    add: 헤더,포스트카드 추가

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,9 +1,68 @@
-import React from 'react';
-// 엔티드, 스탈컴포넌트 import 하시고 사용하시면 됩니다.
+import React from "react";
+import { Card, Col, Row, Select  } from "antd";
+import "antd/dist/antd.css";
+import { PlusCircleOutlined } from "@ant-design/icons";
+import styled from "styled-components";
+
+const AddBtn = styled(PlusCircleOutlined)`
+  position: fixed;
+  font-size: 50px;
+  right: 125px;
+  bottom: 50px;
+  cursor: pointer;
+`;
+
+const postDummyData = [
+  {
+    Post: {
+      postState: "프젝구함",
+    },
+    projectExperience: "1회",
+  },
+];
+
 const Index = (props: any) => {
-    return (
-        <h1>post!</h1>
-    )
-}
+  return (
+    <>
+      <div className="site-card-wrapper" style={{ padding: "0px 140px" }}>
+        <Row gutter={16}>
+          <Col span={6} style={{ background: "blue", padding: "40px 20px" }}>
+            <Card
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+            >
+              <Card.Meta title="Frontend/React" />
+              <br />
+              <p>현재상태 : 프젝구함</p>
+              <p>프로젝트 경험 : 1회</p>
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+        </Row>
+        <AddBtn>
+          <PlusCircleOutlined />
+        </AddBtn>
+      </div>
+    </>
+  );
+};
 
 export default Index;
