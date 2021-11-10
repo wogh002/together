fade5d76048308c4e6ab1cd70b4cfc8aba9887bd
wogh002 <ekekekem159@gmail.com>
2021-11-10 16:50:45 +0900

    fix: 충돌

--- reducers/index.js
+++ reducers/index.js
@@ -3,9 +3,7 @@ import { combineReducers } from 'redux';
 import user from './user';
 import post from './post';
 import zone from './zone';
-
 const rootReducer = (state, action) => {
-    const getServerSideProps = wrapper.getServerSideProps(
     switch (action.type) {
         case HYDRATE:
             console.log('HYDRATE', action);
